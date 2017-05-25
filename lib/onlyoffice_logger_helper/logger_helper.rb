cb7314decbb546e7a7be0c97cf247bc5c0b96569
Pavel Lobashov <shockwavenn@gmail.com>
2017-05-25 14:51:00 +0300

    Fix RuboCop `Performance/Caller` cop

--- lib/onlyoffice_logger_helper/logger_helper.rb
+++ lib/onlyoffice_logger_helper/logger_helper.rb
@@ -1,7 +1,7 @@
 # Module for output stuff in console
 module OnlyofficeLoggerHelper
   def self.log(entry, color_code = nil)
-    caller_name = caller[0].to_s[/\w+.rb/].chomp('.rb')
+    caller_name = caller(1..1).first.to_s[/\w+.rb/].chomp('.rb')
     time_stamp = Time.now.strftime('%T/%d.%m.%y')
     message = "#{time_stamp}    [#{caller_name}] #{entry}"
     color_code ? puts(colorize(message, color_code)) : puts(message)
