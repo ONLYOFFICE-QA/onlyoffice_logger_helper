8a92c523a8aa75cf13349ea790627d0ef76eeed3
Pavel Lobashov <shockwavenn@gmail.com>
2020-05-28 15:05:11 +0300

    Revert "Temp commit"
    This reverts commit 05cda2958d6ff570b04e85611f037aa6e97214ed.

--- lib/onlyoffice_logger_helper/logger_helper.rb
+++ lib/onlyoffice_logger_helper/logger_helper.rb
@@ -4,7 +4,7 @@
 module OnlyofficeLoggerHelper
   def self.log(entry, color_code = nil)
     caller_name = caller(1..1).first.to_s[/\w+.rb/].chomp('.rb')
-    time_stamp = Time.now.strftime("%T/%d.%m.%y")
+    time_stamp = Time.now.strftime('%T/%d.%m.%y')
     message = "#{time_stamp}    [#{caller_name}] #{entry}"
     color_code ? puts(colorize(message, color_code)) : puts(message)
   end
