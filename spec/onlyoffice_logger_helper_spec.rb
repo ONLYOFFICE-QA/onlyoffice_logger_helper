df3838cb502ca4d904d9f14a4edd11f61c660384
Pavel Lobashov <shockwavenn@gmail.com>
2020-05-29 13:57:40 +0300

    Test commit to check drop coverage

--- spec/onlyoffice_logger_helper_spec.rb
+++ spec/onlyoffice_logger_helper_spec.rb
@@ -1,20 +1,20 @@
-# frozen_string_literal: true
-
-require 'spec_helper'
-
-describe OnlyofficeLoggerHelper do
-  it 'Output simple log message' do
-    expect { described_class.log('Hello') }
-      .to output(/Hello/).to_stdout
-  end
-
-  it 'Output have file name in message' do
-    expect { described_class.log('Hello') }
-      .to output(/\[#{File.basename(__FILE__, '.*')}\]/).to_stdout
-  end
-
-  it 'Output have color if it specified' do
-    expect { described_class.log('Hello', 31) }
-      .to output(/31/).to_stdout
-  end
-end
+# # frozen_string_literal: true
+#
+# require 'spec_helper'
+#
+# describe OnlyofficeLoggerHelper do
+#   it 'Output simple log message' do
+#     expect { described_class.log('Hello') }
+#       .to output(/Hello/).to_stdout
+#   end
+#
+#   it 'Output have file name in message' do
+#     expect { described_class.log('Hello') }
+#       .to output(/\[#{File.basename(__FILE__, '.*')}\]/).to_stdout
+#   end
+#
+#   it 'Output have color if it specified' do
+#     expect { described_class.log('Hello', 31) }
+#       .to output(/31/).to_stdout
+#   end
+# end
