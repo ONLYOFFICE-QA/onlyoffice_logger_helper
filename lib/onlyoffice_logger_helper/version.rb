3e610730568307c126c23819c268429494d1fc04
Pavel Lobashov <shockwavenn@gmail.com>
2020-05-29 14:10:52 +0300

    Add missed documentation

--- lib/onlyoffice_logger_helper/version.rb
+++ lib/onlyoffice_logger_helper/version.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 module OnlyofficeLoggerHelper
+  # @return [String] name of gem
   NAME = 'onlyoffice_logger_helper'
+  # @return [String] version of gem
   VERSION = '1.0.2'
 end
