6c72e6db5a025c3692ce2cc0543774ccfc593ce3
Pavel Lobashov <ShockwaveNN@gmail.com>
2021-09-08 17:34:05 +0300

    Release v1.2.0 (#161)

--- lib/onlyoffice_logger_helper/version.rb
+++ lib/onlyoffice_logger_helper/version.rb
@@ -4,5 +4,5 @@ module OnlyofficeLoggerHelper
   # @return [String] name of gem
   NAME = 'onlyoffice_logger_helper'
   # @return [String] version of gem
-  VERSION = '1.1.0'
+  VERSION = '1.2.0'
 end
